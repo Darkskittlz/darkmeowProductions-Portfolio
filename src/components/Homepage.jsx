fd00dd14a07433618874a46a7563167e9d67a070
Nate <natemausert@gmail.com>
2022-02-04 13:53:41 -0500

    style font a lil bit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,12 +7,12 @@ import '../App.css'
 const Homepage = () => {
     return (
         <div id='homeBody'>
-                <div id='homeCard'>
+            <div id='homeCard'>
                 <HomeElement />
-                    <h1 id='homeTitle'>Welcome to the Dev Shop</h1>
-                </div>
+                <h1 id='homeTitle'>Welcome to the Dev Shop</h1>
+            </div>
         </div>
     )
-}
+};
 
 export default Homepage
